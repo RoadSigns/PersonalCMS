4f9c60c5903cc7d8ff15012cc55c6b54b80f315d
Zack <zack@zacklott.co.uk>
2018-01-11 23:35:12 +0000

    NEW: Bootstrap configuration files
    These bootstrap files will contain all of the functions and configuration const that will be used across the whole backend of the application.
    
    [+] config.php
    [+] autoloader.php
    [+] debug.php

---
+++ Bootstrap/config.php
@@ -0,0 +1,7 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Zack
+     * Date: 11/01/2018
+     * Time: 23:31
+     */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+
+    // Required for Start up
+    require ('Bootstrap/config.php'    );
+    require ('Bootstrap/autoloader.php');
+    // Debugging Tools
+    require ('Bootstrap/debug.php');
