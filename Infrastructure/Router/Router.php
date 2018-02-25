e4af956b6090b6598b4f8a6dd1dcfa2cf4635177
Zack <zack@zacklott.co.uk>
2018-02-25 00:20:17 +0000

    Configure the Router to send the user to the correct page from the URL
    The router is now configured so it sends users to the blog posts if they go directly to those and also to the projects if they enter the correct url.
    
    Added the .DS_Store to the gitignore to prevent OSX directory files making it into the repository.
    
    Created the basic configuration file for the Project where I will be declaring consts
    
    [+] Configuartion File
    [+] Routing 
    [+] Prevented OSX files making it into the repository

--- Infrastructure/Router/Router.php
+++ Infrastructure/Router/Router.php
@@ -11,12 +11,21 @@
         public function __construct(RouterEngine $router)
         {
             $this->router = $router;
+            $this->router->setBasePath(BASEPATH);
         }
 
         public function generateRoutes()
         {
             $this->router->map('GET', '/', function(){
                 echo "This is the homepage";
+            });
+
+            $this->router->map('GET', '/project/[:projectName]', function($projectName){
+               echo  "<h1>$projectName</h1>";
+            });
+
+            $this->router->map('GET', '/blog/[:blogPost]', function($blogPost){
+                echo  "<h1>$blogPost</h1>";
             });
         }
 
